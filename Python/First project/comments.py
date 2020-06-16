d56bce5025f9997f17944bce61ac0c7c39a9c60a
VIKULBHA <vikul.bhandari@gmail.com>
2020-06-15 18:19:44 -0700

    pushing my first python file from Pycharm

---
+++ Python/First project/comments.py
@@ -0,0 +1,14 @@
+# THis is a single line comment
+# You can Write comments like this
+print("Hello World")
+
+#Comments here
+print("another print call")
+""" 
+you are in a multiline comment now :)
+That means you can hit enter and separate your comments
+
+Many lines, make sure code is clean.
+"""
+print("You can have comments in the middle of the code")
+print("Adding one more print to show it in the codecast")
